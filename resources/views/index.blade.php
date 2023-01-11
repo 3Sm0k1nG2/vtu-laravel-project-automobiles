df8bfa21077593cd9c80f2132e0a36dc34b340a0
3Sm0k1nG2 <3sm0k1ng2@Gmail.com>
2023-01-11 17:55:39 +0200

    Create simple views for models.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home page</title>
+</head>
+<body style="font-size: 24px">
+    <center><h1>Welcome user</h1></center>
+    <ul>
+        <li><u>Home</u></li>
+        <li><a href="/manufacturers">Manufacturers</a></li>
+        <li><a href="/vmodels">Vmodels</a></li>
+        <li><a href="/vehicles">Vehicles</a></li>
+    </ul>
+</body>
+</html>
